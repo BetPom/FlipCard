0a5581f2dcffd0a501d250542377013e32e0a162
BetPom <b.pomalecka8@gmail.com>
2022-10-14 18:08:47 +0200

    VERSION 1

---
+++ eFlipCard/src/eFlipCard/QuizCardBuilder.java
@@ -0,0 +1,142 @@
+package eFlipCard;
+
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.ScrollPaneConstants;
+
+public class QuizCardBuilder {
+
+	private JTextArea question;
+	private JTextArea answer;
+	private ArrayList<QuizCard> cardList;
+	private JFrame frame;
+	private JButton cardPlay;
+	private JButton nextButton;
+	private QuizCardPlayer cardPlayer;
+	
+	public static void main(String[] args) {
+		QuizCardBuilder builder = new QuizCardBuilder();
+		builder.go();
+	}
+
+	public void go() {
+		frame = new JFrame("Quiz Card Builder");
+		JPanel mainPanel = new JPanel();
+		Font bigFont = new Font("sanserif", Font.BOLD, 24);
+		question = new JTextArea(10, 25);
+		question.setLineWrap(true);
+		question.setWrapStyleWord(true);
+		question.setFont(bigFont);
+
+		JScrollPane qScroller = createScroller(question);
+
+		answer = new JTextArea(10, 25);
+		answer.setLineWrap(true);
+		answer.setWrapStyleWord(true);
+		answer.setFont(bigFont);
+
+		JScrollPane aScroller = createScroller(answer);
+
+		nextButton = new JButton("Next Card");
+		cardPlay = new JButton("card play");
+
+		cardList = new ArrayList<QuizCard>();
+
+		JLabel questionLabel = new JLabel("Question: ");
+		JLabel answerLabel = new JLabel();
+
+		mainPanel.add(questionLabel);
+		mainPanel.add(qScroller);
+
+		mainPanel.add(answerLabel);
+		mainPanel.add(aScroller);
+
+		mainPanel.add(nextButton);
+		nextButton.addActionListener(ActionEvent -> {
+
+			QuizCard card = new QuizCard(question.getText(), answer.getText());
+			cardList.add(card);
+			clearCard();
+		});
+		mainPanel.add(cardPlay);
+		cardPlay.addActionListener(ActionEvent ->{
+			cardPlayer = new QuizCardPlayer();
+			cardPlayer.go();
+			frame.setVisible(false);
+			frame.dispose();
+		});
+
+		JMenuBar menuBar = new JMenuBar();
+		JMenu fileMenu = new JMenu("File");
+		JMenuItem newMenuItem = new JMenuItem("New");
+		JMenuItem saveMenuItem = new JMenuItem("Save");
+		newMenuItem.addActionListener(ActionEvent -> {
+			cardList.clear();
+			clearCard();
+		});
+
+		saveMenuItem.addActionListener(ActionEvent -> {
+			QuizCard card = new QuizCard(question.getText(), answer.getText());
+			cardList.add(card);
+
+			JFileChooser fileSave = new JFileChooser();
+			fileSave.showSaveDialog(frame);
+			saveFile(fileSave.getSelectedFile());
+		});
+
+		fileMenu.add(newMenuItem);
+		fileMenu.add(saveMenuItem);
+		menuBar.add(fileMenu);
+
+		frame.setJMenuBar(menuBar);
+		frame.setVisible(true);
+		frame.setSize(600, 800);
+		frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+
+	}
+
+	private JScrollPane createScroller(JTextArea textArea) {
+		JScrollPane scroller = new JScrollPane(textArea);
+		scroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		return scroller;
+	}
+
+	private void saveFile(File file) {
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+			for (QuizCard card : cardList) {
+				writer.write(card.getQuestion() + "/");
+				writer.write(card.getAnswer() + "\n");
+			}
+		} catch (IOException e) {
+			System.out.println("Couldn't write the cardList out");
+			e.printStackTrace();
+		}
+
+	}
+
+	private void clearCard() {
+		question.setText("");
+		answer.setText("");
+		question.requestFocus();
+	}
+
+}
